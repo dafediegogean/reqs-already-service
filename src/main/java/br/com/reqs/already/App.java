a5d6f0b902c9cd5f0c3e77ef4c2867e3f1d1246e
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-11-16 21:27:40 -0300

    Update App.java

--- src/main/java/br/com/reqs/already/App.java
+++ src/main/java/br/com/reqs/already/App.java
@@ -1,8 +1,8 @@
 package br.com.reqs.already;
-
 /**
- * Hello world!
- *
+ * @author <a href="mailto:dafediegogean@gmail.com">Diego Gean da Fé</a>
+ * @version
+ * @since 24 de out de 2020, 15:25:10
  */
 public class App {
     public static void main( String[] args ) {
