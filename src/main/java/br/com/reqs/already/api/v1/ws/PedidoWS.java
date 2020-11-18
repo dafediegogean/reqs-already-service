c7b8ca7fbd58b921ca6c7bb0dc7af4f6607e4eae
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-11-17 22:29:47 -0300

    Atualizado parcialmente PedidoWS

--- src/main/java/br/com/reqs/already/api/v1/ws/PedidoWS.java
+++ src/main/java/br/com/reqs/already/api/v1/ws/PedidoWS.java
@@ -1,50 +1,29 @@
 package br.com.reqs.already.api.v1.ws;
 
-import java.util.List;
-
-import javax.enterprise.inject.Produces;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.xml.bind.annotation.XmlElement;
 
-import br.com.reqs.already.domain.model.Pedido;
+import br.com.reqs.already.domain.dto.PedidoDTO;
 
+/**
+ * Classe de exposição de WebService, e das demais operations para
+ * buscar, criar, atualizar e remover de Pedidos.
+ * 
+ * @author <a href="mailto:dafediegogean@gmail.com">Diego Gean da Fé</a>
+ * @version
+ * @since 30 de out de 2020, 21:42:10
+ */
 @WebService
 public class PedidoWS {
-	
-	@Produces
-	@PersistenceContext
-    private EntityManager em;
-    
+	  
 	@WebMethod(operationName = "getPedido")
 	@WebResult(name = "pedido")
-	public Pedido getPedido(@WebParam(name = "id") @XmlElement(required = true ) Long id) {
-		Pedido pedido = new Pedido();
-		
-		Query query = em.createNativeQuery("select * from pedido");
-		List<Object[]> pedidos = query.getResultList();
-		System.out.println(pedidos);
-		for (Object[] object : pedidos) {
-			System.out.println(object);
-			br.com.reqs.already.domain.entity.Pedido p = new br.com.reqs.already.domain.entity.Pedido();
-			p.setId(Long.valueOf(object[0].toString()));
-			p.setDescricao(object[1].toString());
-			System.out.println(p);
-		}
-		
-		if (id == 1L) {
-			pedido.setId(1L);
-			pedido.setNome("Compra Kit Camiseta Promo");
-		} else {
-			pedido.setId(2L);
-			pedido.setNome("Compra Celular Samsung Galaxy");
-		}
-		return pedido;
+	public PedidoDTO getPedido(@WebParam(name = "id") @XmlElement(required = true ) Long id) {
+	
+		return null;
 	}
 	
 }
