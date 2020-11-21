6cb5c5b863fb9c501cc89c750d3f8aafe05d6890
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-11-21 20:51:32 -0300

    Criado ProdutoWS

---
+++ src/main/java/br/com/reqs/already/api/v1/ws/ProdutoWS.java
@@ -0,0 +1,16 @@
+package br.com.reqs.already.api.v1.ws;
+
+import javax.jws.WebService;
+
+/**
+ * Classe de exposição de WebService, e das demais operations para
+ * buscar, criar, atualizar e remover de Produto.
+ * 
+ * @author <a href="mailto:dafediegogean@gmail.com">Diego Gean da Fé</a>
+ * @version
+ * @since 21 de nov de 2020, 20:48:10
+ */
+@WebService
+public class ProdutoWS {
+
+}
