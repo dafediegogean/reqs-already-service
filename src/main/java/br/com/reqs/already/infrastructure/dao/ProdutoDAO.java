b89247692719430b20b8e93615d454b88157924c
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-11-21 22:05:01 -0300

    Estendido AbstractJPA

--- src/main/java/br/com/reqs/already/infrastructure/dao/ProdutoDAO.java
+++ src/main/java/br/com/reqs/already/infrastructure/dao/ProdutoDAO.java
@@ -2,10 +2,11 @@ package br.com.reqs.already.infrastructure.dao;
 
 import javax.inject.Named;
 
+import br.com.reqs.already.core.jpa.AbstractJPA;
 import br.com.reqs.already.domain.dto.ProdutoDTO;
 
 @Named
-public class ProdutoDAO {
+public class ProdutoDAO extends AbstractJPA {
 	
 	public ProdutoDTO getProdutoById(Long id) {
 		System.out.println("ProdutoDAO");
