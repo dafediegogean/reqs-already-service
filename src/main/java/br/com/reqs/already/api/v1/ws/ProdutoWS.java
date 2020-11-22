9ea42e35da4aab79782754bd0425d6e305bf1ef1
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-11-21 22:02:33 -0300

    Injetado serviço ProdutoService

--- src/main/java/br/com/reqs/already/api/v1/ws/ProdutoWS.java
+++ src/main/java/br/com/reqs/already/api/v1/ws/ProdutoWS.java
@@ -1,6 +1,14 @@
 package br.com.reqs.already.api.v1.ws;
 
+import javax.inject.Inject;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+
+import br.com.reqs.already.domain.dto.ProdutoDTO;
+import br.com.reqs.already.infrastructure.service.ProdutoService;
 
 /**
  * Classe de exposição de WebService, e das demais operations para
@@ -12,5 +20,21 @@ import javax.jws.WebService;
  */
 @WebService
 public class ProdutoWS {
-
+	
+	@Inject
+	private ProdutoService produtoService;
+	
+	/**
+	 * Método getProduto(Long id), recebe como parâmetro o id, para
+	 * buscar um produto pelo id. 
+	 * 
+	 * @param id
+	 * @return ProdutoDTO
+	 */
+	@WebMethod(operationName = "getProduto")
+	@WebResult(name = "produto")
+	public ProdutoDTO getProduto(@WebParam(name = "id") @XmlElement(required = true) Long id) {
+		produtoService.getProduto(id);
+		return null;
+	}
 }
