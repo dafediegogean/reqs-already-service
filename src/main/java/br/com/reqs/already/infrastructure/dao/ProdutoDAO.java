3170fda719a7f512d726a84e7786769dd44fc9de
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-12-31 20:43:27 -0300

    Atualizado o DAO de produto

--- src/main/java/br/com/reqs/already/infrastructure/dao/ProdutoDAO.java
+++ src/main/java/br/com/reqs/already/infrastructure/dao/ProdutoDAO.java
@@ -1,21 +1,35 @@
 package br.com.reqs.already.infrastructure.dao;
 
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.reqs.already.core.jpa.AbstractJPA;
-import br.com.reqs.already.domain.dto.ProdutoDTO;
+import br.com.reqs.already.domain.entity.Produto;
 
 /**
+ * Classe que representa a camada de persistência (DAO), e que atende a demais
+ * ao escopo de produto.
  * 
- * @author diego
- *
+ * @author <a href="mailto:dafediegogean@gmail.com">Diego Gean da Fé</a>
+ * @version
+ * @since 21 de nov de 2020, 22:03:10
  */
 @Named
 public class ProdutoDAO extends AbstractJPA {
 	
-	public ProdutoDTO getProdutoById(Long id) {
-		System.out.println("ProdutoDAO");
-		return null;
+	@PersistenceContext
+	private EntityManager em;
+	
+	/**
+	 * Método findById(Long id), recebe como parâmetro o id do tipo
+	 * java.lang.Long, e busca o produto em base de dados pelo id.
+	 * 
+	 * @param id
+	 * @return produto
+	 */
+	public Produto findById(Long id) {
+		return em.find(Produto.class, id);
 	}
 	
 }
