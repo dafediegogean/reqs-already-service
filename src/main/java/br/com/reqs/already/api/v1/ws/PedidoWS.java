a4f2fb893cf84f970106f8a13b21d1b778a64643
Diego Gean da Fé <diegogeandafe@gmail.com>
2021-01-17 14:11:14 -0300

    Atualizado Web service de Produto

--- src/main/java/br/com/reqs/already/api/v1/ws/PedidoWS.java
+++ src/main/java/br/com/reqs/already/api/v1/ws/PedidoWS.java
@@ -4,6 +4,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 import javax.xml.bind.annotation.XmlElement;
 
 import br.com.reqs.already.domain.dto.PedidoDTO;
@@ -17,6 +21,7 @@ import br.com.reqs.already.domain.dto.PedidoDTO;
  * @since 30 de out de 2020, 21:42:10
  */
 @WebService
+@SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
 public class PedidoWS {
 	  
 	@WebMethod(operationName = "getPedido")
--- src/main/java/br/com/reqs/already/api/v1/ws/ProdutoWS.java
+++ src/main/java/br/com/reqs/already/api/v1/ws/ProdutoWS.java
@@ -7,6 +7,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 import javax.xml.bind.annotation.XmlElement;
 
 import br.com.reqs.already.core.exception.AuthorizationException;
@@ -25,22 +29,16 @@ import br.com.reqs.already.infrastructure.service.ProdutoService;
  * @since 21 de nov de 2020, 20:48:10
  */
 @WebService
+@SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL, parameterStyle = ParameterStyle.WRAPPED)
 public class ProdutoWS {
 	
 	@Inject
 	private ProdutoService produtoService;
 	
-	/**
-	 * Método getAll(Token token), recebe como parâmetro o token para autorização de requisição,
-	 * caso válido, busca e retorna todos os produtos cadastrados 
-	 * na base de dados.
-	 * 
-	 * @param token
-	 * @return ListaProdutoDTO
-	 */
-	@WebMethod(operationName = "getAll")
+	@WebMethod(operationName = "todosOsProdutos")
 	@WebResult(name = "produtos")
-	public ListaProdutoDTO getAll(@WebParam(name = "token", header = true) Token token) {
+	public ListaProdutoDTO getAll(@WebParam(name= "token", header = true) Token token, 
+			@WebParam(name="paginacao") @XmlElement(required = false) boolean paginacao) {
 		boolean isAuthenticated = AuthorizationValidator.tokenValidator(token);
 		
 		if (!isAuthenticated) {
@@ -52,16 +50,9 @@ public class ProdutoWS {
 		return new ListaProdutoDTO(produtos);
 	}
 	
-	/**
-	 * Método getProduto(Long id), recebe como parâmetro o id, para
-	 * buscar um produto pelo id. 
-	 * 
-	 * @param id
-	 * @return ProdutoDTO
-	 */
 	@WebMethod(operationName = "getProdutoById")
 	@WebResult(name = "produto")
-	public ProdutoDTO getProduto(@WebParam(name = "token", header = true) Token token, 
+	public ProdutoDTO getProdutoById(@WebParam(name = "token", header = true) Token token, 
 			@WebParam(name = "id") @XmlElement(required = true) Long id) {
 		boolean isAuthenticated = AuthorizationValidator.tokenValidator(token);
 		
@@ -70,4 +61,5 @@ public class ProdutoWS {
 		}
 		return produtoService.getProdutoById(id);
 	}
+	
 }
